90a1028a78f4616d669b003939aaa4569c0ee6cc
jennybc <jenny@stat.ubc.ca>
2013-10-11 12:53:35 -0700

    Code that did cleaning in previous commit

--- 01_clean.R
+++ 01_clean.R
@@ -1,2 +1,4 @@
-lotrDat <- read.delim("lotr_wordsSpoken.tsv")
+lotrDat <- read.delim("Words Spoken, By Character, Race, & Scene, in the Extended Editi.txt")
 str(lotrDat)
+write.table(lotrDat, "lotr_wordsSpoken.tsv", quote = FALSE,
+            sep = "\t", row.names = FALSE)
